a682356717169fee623a9cb73c7a850c875af4a6
Emiliano Barberis <emibarberis26@gmail.com>
2022-08-18 16:19:23 -0300

    funcionalidad PWA

--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -9,10 +9,43 @@ function CreateTodo({mode , setMode , dispatch , tasks }) {
  
  const [icon , setIcon] = useState(imageSun)
  const [text , setText] = useState('')   
- 
+ const [isReadyForInstall, setIsReadyForInstall] = React.useState(false);
+
+useEffect(() => {
+  window.addEventListener("beforeinstallprompt", (event) => {
+    // Prevent the mini-infobar from appearing on mobile.
+    event.preventDefault();
+    console.log("👍", "beforeinstallprompt", event);
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+    // Remove the 'hidden' class from the install button container.
+    setIsReadyForInstall(true);
+  });
+}, []);
+
  useEffect(()=>{
  
 },[icon ])
+
+async function downloadApp() {
+  console.log("👍", "butInstall-clicked");
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    // The deferred prompt isn't available.
+    console.log("oops, no prompt event guardado en window");
+    return;
+  }
+  // Show the install prompt.
+  promptEvent.prompt();
+  // Log the result
+  const result = await promptEvent.userChoice;
+  console.log("👍", "userChoice", result);
+  // Reset the deferred prompt variable, since
+  // prompt() can only be called once.
+  window.deferredPrompt = null;
+  // Hide the install button.
+  setIsReadyForInstall(false);
+}
 
  const handleMode = ()=>{
     if(icon === imageMoon){
@@ -43,6 +76,7 @@ function CreateTodo({mode , setMode , dispatch , tasks }) {
  }
   return (
     <div className="header" style={mode === 'dark' ? { backgroundImage:`url(${darkImage})`} : { backgroundImage:`url(${lightImage})`}}>
+      {isReadyForInstall && <button className='btn-descarga' onClick={downloadApp}>Descarga la App</button>}
     <nav>
       <div className="nav-items">
         <div className="logo">
